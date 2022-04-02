465d9301d689630e391ce3e838a2443bf1ba0ca2
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-02 14:10:09 +0200

    Finish CoC modal

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,24 @@
+/* -----  Code for the survey buttons ------ */
+
 const buttonSet = document.querySelectorAll('.survey');
 Array.from(buttonSet).forEach(element => element.addEventListener("click", makeActive));
 
 function makeActive(click) {
   click.target.classList.toggle("survey__checked");
+}
+
+
+/* ----- Code for the CoC modal --------- */
+
+document.querySelector(".coc-link").addEventListener("click", openCoC);
+document.querySelector(".coc-agree").addEventListener("click", agreeCoC);
+
+function openCoC(){ 
+  document.querySelector(".coc-modal").style.display="block";
+}
+
+function agreeCoC() {
+  const agreeBtn = document.getElementById("cocAgreeCheck");
+  document.querySelector(".coc-modal").style.display="none";
+  agreeBtn.setAttribute("checked", "");
 }
