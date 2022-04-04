dd8736747e31f9678907de56de4608f54fddbaf6
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-04 07:14:56 +0200

    Add Code of Conduct button

--- assets/main.js
+++ assets/main.js
@@ -12,6 +12,7 @@ function makeActive(click) {
 
 document.querySelector(".coc-link").addEventListener("click", openCoC);
 document.querySelector(".coc-agree").addEventListener("click", agreeCoC);
+document.querySelector(".coc-close").addEventListener("click", closeCoC);
 
 function openCoC(){ 
   document.querySelector(".coc-modal").style.display="block";
@@ -21,4 +22,8 @@ function agreeCoC() {
   const agreeBtn = document.getElementById("cocAgreeCheck");
   document.querySelector(".coc-modal").style.display="none";
   agreeBtn.setAttribute("checked", "");
+}
+
+function closeCoC() {
+  document.querySelector(".coc-modal").style.display="none";
 }
