417269786f262444f55f3037173dd4252c475d26
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-04 12:10:12 +0200

    Begin process

--- assets/survey.js
+++ assets/survey.js
@@ -1,6 +1,11 @@
 /* -----  Code for the survey buttons ------ */
 
 const buttonSet = document.querySelectorAll('.survey');
+const personal = document.querySelector(".survey-personal");
+const professional = document.querySelector(".survey-professional");
+const both = document.querySelector(".survey.both");
+const submit = document.querySelector(".submit-survey");
+
 Array.from(buttonSet).forEach(element => element.addEventListener("click", makeActive));
 
 function makeActive(click) {
