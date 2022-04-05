666e927d01310b6490f138d8c4ec4a04b67d966b
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-05 11:35:48 +0200

    Implement page transition

--- assets/scripts/dashDelay.js
+++ assets/scripts/dashDelay.js
@@ -1,3 +1,10 @@
-
+setTimeout(function() {
+  document.querySelector("body").removeAttribute("class");
+  document.querySelector("main").innerHTML = `<img src="./assets/icons/logo_with_text.svg" alt="ShesWithUs" class="logo">
+  <section class="top-space pre-dash">
+    <h1>Your personalized dashboard is ready!</h1>
+    <a href="./profile1AccountInfo.html" class="button">Join the community</a>
+  </section>`;
+}, 3000);
 
 
--- assets/scripts/splashDelay.js
+++ assets/scripts/splashDelay.js
@@ -1,3 +1,25 @@
+setTimeout(function() {
+  document.querySelector("body").innerHTML = `<main>
+  <img src="./assets/icons/logo_with_text.svg" alt="ShesWithUs" class="logo">
+  <section class="slide slide1 slide-container">
+    <h1>She is not afraid to ask for help.</h1>
+    <p class="txt-light">We can always use some support in all areas of our lives, whether personal or professional.
+      Post your questions and get support.
+    <div class="slide-button-container">
+      <div class="slide-button"></div>
+      <div class="slide-button"></div>
+      <div class="slide-button"></div>
+    </div>
+  </section>
 
+  <section class="link-group">
+    <a href="./onboardSurvey1.html" class="button">Join the community</a>
+    <a href="./login.html" class="login">I'm already a member</a>
+  </section>
+
+
+</main>
+<script src="./assets/scripts/main.js"></script>`
+}, 3000);
 
 
