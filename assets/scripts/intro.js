9f12b988bc2aae6cc017b50e8d9dc8c3824fcfeb
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-06 10:13:09 +0200

    Commit failed attempt

--- assets/scripts/intro.js
+++ assets/scripts/intro.js
@@ -7,7 +7,9 @@ const logoGroup = document.querySelectorAll(".intro");
 const button1 = document.querySelector(".sb1");
 const button2 = document.querySelector(".sb2");
 const button3 = document.querySelector(".sb3");
-const slide = document.querySelector(".slide");
+const slide1 = document.querySelector(".slide1");
+const slide2 = document.querySelector(".slide2");
+const slide3 = document.querySelector(".slide3");
 
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -46,50 +48,67 @@ button2.addEventListener("click", loadSecondSlide);
 button3.addEventListener("click", loadThirdSlide);
 
 function loadFirstSlide() {
-  if (slide.classList.contains("slide1"))
+  if (slide1.classList.contains("slide-center"))
   return;
-  else {
-    button1.classList.add("blue");
-    button2.classList.remove("blue");
-    button3.classList.remove("blue");
-    slide.innerHTML = `<h1>She is not afraid to ask for help.</h1>
-    <p class="txt-light">We can always use some support in all areas of our lives, whether personal or
-      professional.
-      Post your questions and get support.</p>`;
-    slide.classList.add("slide1");
-    slide.classList.remove("slide2");
-    slide.classList.remove("slide3");
-  }
-}
-
-function loadSecondSlide() {
-  if (slide.classList.contains("slide2"))
-  return;
-  else {
-    button1.classList.remove("blue");
+  else if (slide2.classList.contains("slide-left")) {
+    slide3.classList.replace("slide-center", "slide-right");
+    slide2.classList.replace("slide-left", "slide-center");
     button2.classList.add("blue");
     button3.classList.remove("blue");
-    slide.innerHTML = `<h1>She is supportive.</h1>
-    <p class="txt-light">Show your strengths by stepping up to guide one another.</p>`;
-    slide.classList.remove("slide1");
-    slide.classList.add("slide2");
-    slide.classList.remove("slide3");
+    setTimeout(() => {
+      slide2.classList.replace("slide-center", "slide-right");
+      slide1.classList.replace("slide-left", "slide-center");
+      button1.classList.add("blue");
+      button2.classList.remove("blue");
+    }, 500);
   }
-}
+  else {
+    slide2.classList.replace("slide-center", "slide-right");
+    slide1.classList.replace("slide-left", "slide-center");
+    button1.classList.add("blue");
+    button2.classList.remove("blue");
+  }
+};
+
+
+function loadSecondSlide() {
+  if (slide2.classList.contains("slide-center"))
+  return;
+  else if (slide2.classList.contains("slide-left")) {
+    slide3.classList.replace("slide-center", "slide-right");
+    slide2.classList.replace("slide-left", "slide-center");
+    button2.classList.add("blue");
+    button3.classList.remove("blue");
+  }
+  else {
+    slide1.classList.replace("slide-center", "slide-left");
+    slide2.classList.replace("slide-left", "slide-center");
+    button1.classList.remove("blue");
+    button2.classList.add("blue");
+  }
+};
 
 function loadThirdSlide() {
-  if (slide.classList.contains("slide3"))
+  if (slide3.classList.contains("slide-center"))
   return;
-  else {
+  else if (slide2.classList.contains("slide-right")) {
+    slide1.classList.replace("slide-center", "slide-left");
+    slide2.classList.replace("slide-right", "slide-center");
+    button2.classList.add("blue");
     button1.classList.remove("blue");
-    button2.classList.remove("blue");
-    button3.classList.add("blue");
-    slide.innerHTML = `<h1>She is connected.</h1>
-    <p class="txt-light">Establish meaningful relationships by being part of a supportive community.</p>`;
-    slide.classList.remove("slide1");
-    slide.classList.remove("slide2");
-    slide.classList.add("slide3");
+    setTimeout(() => {
+      slide2.classList.replace("slide-center", "slide-left");
+      slide3.classList.replace("slide-right", "slide-center");
+      button3.classList.add("blue");
+      button2.classList.remove("blue");
+    }, 500);
   }
-}
+  else {
+    slide2.classList.replace("slide-center", "slide-left");
+      slide3.classList.replace("slide-right", "slide-center");
+      button3.classList.add("blue");
+      button2.classList.remove("blue");
+  }
+};
 
 
