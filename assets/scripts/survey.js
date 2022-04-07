44995722cb4355e1ae557162bfc2f3665a3c29bb
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-07 10:52:43 +0200

    Complete first pass

--- assets/scripts/survey.js
+++ assets/scripts/survey.js
@@ -1,10 +1,11 @@
 // Here I grab all of the elements that I am going to reference
-
-let buttonSet = document.querySelectorAll('.survey');
 const personal = document.querySelector(".select-personal");
 const professional = document.querySelector(".select-professional");
 const both = document.querySelector(".select-both");
-const submit = document.querySelector(".submit-survey");
+
+personal.addEventListener("click", updatePersonal);
+professional.addEventListener("click", updateProfessional);
+both.addEventListener("click", updateBoth);
 
 // And here are all of the elements that are going to be changed
 
@@ -17,7 +18,7 @@ const buttonSwap = document.querySelector(".btn-container");
 
 // I am sure that there is a better way to do all of this, but I'm afraid I don't know it.
 
-const personalSurveyList = `<h2>Personal</h2>
+const personalSurveyList = `
 <ul class="survey-container-grid">
   <li class="button survey two-line">Time<br> Management</li>
   <li class="button survey">Mental Health</li>
@@ -29,7 +30,7 @@ const personalSurveyList = `<h2>Personal</h2>
   <li class="button survey">Finance</li>
   <li class="button survey">Parenting</li>
 </ul>`;
-const professionalSurveyList = `<h2>Professional</h2>
+const professionalSurveyList = `
 <ul class="survey-container-grid">
   <li class="button survey">Career Transition</li>
   <li class="button survey two-line">Salary<br> Negotiation</li>
@@ -40,41 +41,80 @@ const professionalSurveyList = `<h2>Professional</h2>
   <li class="button survey two-line">Conflict<br>Management</li>
 </ul>`
 
-createListeners();
 
 // We make an array from the survey options and apply a new style when they're checked.  This will also apply to the secondary survey items, but we'll have to call it again.
 
 function createListeners() {
+  let buttonSet = document.querySelectorAll(".survey");
   Array.from(buttonSet).forEach(element => element.addEventListener("click", makeActive));
 
   function makeActive(click) {
-    click.target.classList.toggle("survey__checked");
+    click.target.classList.remove("survey");
   }
 };
 
 
 // Here's where we start the process of updating the page's contents depending on the choices made in the initial survey.
 
-submit.addEventListener("click", updateSurvey);
 
-function updateSurvey() {
-  baseSurvey.style.display = "none";
-  if (both.classList.contains("survey__checked") || (personal.classList.contains("survey__checked") && professional.classList.contains("survey__checked"))) {
+function updatePersonal() {
+  personal.classList.remove("survey");
+  setTimeout(() => {
+    baseSurvey.style.display="none";
+    headerTxt.innerText = `Which area(s) of your personal life would you want to improve on the most?`;
+    personalOptions.innerHTML = personalSurveyList;
+    buttonSwap.innerHTML = `<a href="./onboardSurvey4.html" class="button btn-small">Continue</a>`;
+    pTxt.classList.remove("left");
+  }, 500);
+
+  buttonSet = document.querySelectorAll('.survey');
+  createListeners();
+};
+
+function updateProfessional() {
+  professional.classList.remove("survey");
+  setTimeout(() => {
+    baseSurvey.style.display="none";
+    headerTxt.innerText = `Which area(s) of your professional life would you want to improve on the most?`;
+    professionalOptions.innerHTML = professionalSurveyList;
+    buttonSwap.innerHTML = `<a href="./onboardSurvey4.html" class="button btn-small">Continue</a>`;
+    pTxt.classList.remove("left");
+  }, 500);
+
+  buttonSet = document.querySelectorAll('.survey');
+  createListeners();
+};
+
+function updateBoth() {
+  both.classList.remove("survey");
+  setTimeout(() => {
+    baseSurvey.style.display="none";
     headerTxt.innerText = `Which area(s) of your personal or professional life would you want to improve on the most?`;
     personalOptions.innerHTML = personalSurveyList;
     professionalOptions.innerHTML = professionalSurveyList;
-  }
-  else if (personal.classList.contains("survey__checked")) {
-    headerTxt.innerText = `Which area(s) of your personal life would you want to improve on the most?`;
-    personalOptions.innerHTML = personalSurveyList;
-  }
-  else {
-    headerTxt.innerText = `Which area(s) of your professional life would you want to improve on the most?`;
-    professionalOptions.innerHTML = professionalSurveyList;
-  }
-  pTxt.innerHTML = `Choose up to three.<br>You can edit them later.`;
-  buttonSwap.innerHTML = `<a href="./onboardSurvey4.html" class="button btn-small">Continue</a>`;
-  pTxt.classList.remove("left");
+    buttonSwap.innerHTML = `<a href="./onboardSurvey4.html" class="button btn-small">Continue</a>`;
+    pTxt.classList.remove("left");
+  }, 500);
+
   buttonSet = document.querySelectorAll('.survey');
   createListeners();
-}
+};
+
+  // if (both.classList.contains("survey__checked") || (personal.classList.contains("survey__checked") && professional.classList.contains("survey__checked"))) {
+  //   headerTxt.innerText = `Which area(s) of your personal or professional life would you want to improve on the most?`;
+  //   personalOptions.innerHTML = personalSurveyList;
+  //   professionalOptions.innerHTML = professionalSurveyList;
+  // }
+  // else if (personal.classList.contains("survey__checked")) {
+  //   headerTxt.innerText = `Which area(s) of your personal life would you want to improve on the most?`;
+  //   personalOptions.innerHTML = personalSurveyList;
+  // }
+  // else {
+  //   headerTxt.innerText = `Which area(s) of your professional life would you want to improve on the most?`;
+  //   professionalOptions.innerHTML = professionalSurveyList;
+  // }
+  // pTxt.innerHTML = `Choose up to three.<br>You can edit them later.`;
+  // buttonSwap.innerHTML = `<a href="./onboardSurvey4.html" class="button btn-small">Continue</a>`;
+  // pTxt.classList.remove("left");
+  // buttonSet = document.querySelectorAll('.survey');
+  // createListeners();
